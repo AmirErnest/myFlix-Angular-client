416b45dbd1ea617bab153f5350458de8feee8945
Amir Ernest <amir.m.ernest@gmail.com>
2022-02-04 21:13:26 +0100

    remove user scss extension

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -64,7 +64,7 @@ export class FetchApiDataService {
   public userRegistration(userDetails: any): Observable<any> {
     //console.log(userDetails);
     return this.http
-      .post(apiUrl + 'users/register', userDetails)
+      .post(apiUrl + 'users', userDetails)
       .pipe(catchError(this.handleError));
   }
 
